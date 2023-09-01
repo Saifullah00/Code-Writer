0dc6941c7cef8ce96ba89d48a2472abdbe23dc27
Saifullah <saifu3140@example.com>
2023-09-01 15:11:49 +0530

    deploying

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-deploy/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
