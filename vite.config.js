52c51f33a596d07f4d2987947f3fee40a5e80005
Saifullah <saifu3140@example.com>
2023-09-06 06:32:39 +0530

    ADDED FEW LINES IN VITE

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-demo/',
+  // base: '/vite-deploy-demo/',
+  base: '/Code-Writer/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
